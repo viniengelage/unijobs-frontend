d998663f4c1eeecdd7c23f25d9edc927e3d166e1
Thiago Alves <thiago_marketingdigital@hotmail.com>
2020-07-09 17:10:31 -0300

    rota /product adicionada

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import LandingPage from '../pages/LandingPage';
+import Product from '../pages/Product';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={LandingPage} />
+    <Route path="/product" exact component={Product} />
   </Switch>
 );
 
