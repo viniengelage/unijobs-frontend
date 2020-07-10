55857f7ff6566ded14eb3a8f75b4ae72a6b05be0
vinicios <viniciosvalensuela@gmail.com>
2020-07-10 16:23:18 -0300

    footer and header add to product page

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -10,6 +10,9 @@ import {
   Price,
 } from './styles';
 
+import Banner from '../../components/Banner';
+import Footer from '../../components/Footer';
+
 interface IItem {
   name: string;
   description: string;
@@ -46,50 +49,54 @@ const Product: React.FC = () => {
   }, []);
 
   return (
-    <Content>
-      <Item>
-        <Images>
-          <p>600 x 300px</p>
-        </Images>
+    <>
+      <Banner />
+      <Content>
+        <Item>
+          <Images>
+            <p>600 x 300px</p>
+          </Images>
 
-        <InfoContact>
-          <p>
-            Por <strong>{post?.author}</strong>
-          </p>
-          <h1>{post?.name}</h1>
+          <InfoContact>
+            <p>
+              Por <strong>{post?.author}</strong>
+            </p>
+            <h1>{post?.name}</h1>
 
-          <Price>
-            <p>Preço: </p>
-            <span>R$ {post?.price?.toFixed(2)}</span>
-          </Price>
+            <Price>
+              <p>Preço: </p>
+              <span>R$ {post?.price?.toFixed(2)}</span>
+            </Price>
 
-          <Buttons>
-            <button type="button">
-              <FaWhatsapp />
-              Enviar Mensagem
-            </button>
-            <button type="button">
-              <FaEnvelope />
-              Enviar Mensagem
-            </button>
-          </Buttons>
-        </InfoContact>
+            <Buttons>
+              <button type="button">
+                <FaWhatsapp />
+                Enviar Mensagem
+              </button>
+              <button type="button">
+                <FaEnvelope />
+                Enviar Mensagem
+              </button>
+            </Buttons>
+          </InfoContact>
 
-        <Description>
-          <h4>Descrição</h4>
-          <p>
-            - Desbloqueado. - Tela PLS de 6.2. <br />
-            - Câmeras traseiras de 13Mpx/2Mpx. - Câmera frontal de 8Mpx. <br />
-            - Processador MediaTek MT6762 Helio P22 Octa-Core de 2GHz com 2GB de
-            RAM. <br />
-            - Bateria de 4000mAh. <br />
-            - Memória interna 32GB. Adequado para cartão SD de 512GB. <br />
-            - Sistema operacional Android 9.0 Pie. - Com sensor de impressão
-            digital. <br />
-          </p>
-        </Description>
-      </Item>
-    </Content>
+          <Description>
+            <h4>Descrição</h4>
+            <p>
+              - Desbloqueado. - Tela PLS de 6.2. <br />- Câmeras traseiras de
+              13Mpx/2Mpx. - Câmera frontal de 8Mpx. <br />
+              - Processador MediaTek MT6762 Helio P22 Octa-Core de 2GHz com 2GB
+              de RAM. <br />
+              - Bateria de 4000mAh. <br />
+              - Memória interna 32GB. Adequado para cartão SD de 512GB. <br />
+              - Sistema operacional Android 9.0 Pie. - Com sensor de impressão
+              digital. <br />
+            </p>
+          </Description>
+        </Item>
+      </Content>
+      <Footer />
+    </>
   );
 };
 
