ef1c65ff2247e35e263fb64d00c4f6b8b9b52312
Vinicius Oliveira de Freitas <vinicius.vof@outlook.com>
2020-08-21 20:47:37 -0300

    Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import { FiFacebook, FiTwitter, FiYoutube, FiInstagram } from 'react-icons/fi';
 
-import { Container, Content } from './styles';
+import { Container, Navigation, Midias, Copyright } from './styles';
+import Logo from '../../assets/logo-uniamerica.png';
 
 const Footer: React.FC = () => (
-  <Container>
-    <Content>
-      <FiFacebook size={60} type="outline" />
-      <FiTwitter size={60} />
-      <FiYoutube size={60} />
-      <FiInstagram size={60} />
-    </Content>
-    <p>© 2020 UniJobs - Todos os direitos reservados</p>
-  </Container>
+  <>
+    <Container>
+      <img src={Logo} alt="Uniamérica" />
+      <Navigation>
+        <li><Link to="/link">Ajuda e contato</Link></li>
+        <li><Link to="/link">Mapa do site</Link></li>
+        <li><Link to="/link">Noções básicas de como comprar</Link></li>
+        <li><Link to="/link">Vender na Unijobs</Link></li>
+      </Navigation>
+      <Midias>
+        <FiFacebook size={45} type="outline" />
+        <FiTwitter size={45} />
+        <FiYoutube size={45} />
+        <FiInstagram size={45} />
+      </Midias>
+    </Container>
+    <Copyright>
+      © 2020 UniJobs - Todos os direitos reservados
+    </Copyright>
+  </>
 );
 
 export default Footer;
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,39 +1,61 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  margin-top: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  p {
-    margin: 40px auto;
-    font-style: italic;
-    font-weight: 300;
-    font-size: 18px;
-    line-height: 22px;
-  }
-`;
-export const Content = styled.div`
+export const Container = styled.footer`
   display: flex;
   align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 250px;
+  justify-content: space-around;
+  height: 150px;
   background: #eeeeee;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.05);
-
-  svg {
-    color: #0e346a;
-    stroke-width: 1px;
-
-    &:hover {
-      color: #65ad2f;
-    }
-
-    & + svg {
-      margin-left: 65px;
-    }
+  width: 100%;
+  padding-right: 20px;
+  img{
+    height: 80%;
   }
+`;
+
+export const Navigation = styled.ul`
+   list-style: none;
+   display: flex;
+   li{
+      display: flex;
+      justify-content: space-between;
+      padding: 20px;
+     a{
+       text-decoration: none;
+       color: #555;
+       font-weight: 500;
+     }
+     a:hover{
+       color: #65AD2F;
+     }
+   }
+`;
+
+export const Midias = styled.section`
+   display: flex;
+   align-items: center;
+   justify-content: space-around;
+   svg {
+     color: #0e346a;
+     stroke-width: 1px;
+     transition: color 200ms;
+     margin: 10px;
+
+     &:hover {
+       color: #65ad2f;
+       cursor: pointer;
+     }
+
+
+   }
+
+`;
+export const Copyright = styled.p`
+ margin: 40px auto;
+ font-style: italic;
+ font-weight: 300;
+ font-size: 18px;
+ line-height: 22px;
+ text-align: center;
 `;
