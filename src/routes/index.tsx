0d6a12e247d189a56094a90be3d138ba6db964db
Vinicius Oliveira de Freitas <vinicius.vof@outlook.com>
2020-08-24 19:01:13 -0300

    Correção

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,6 @@ import LandingPage from '../pages/LandingPage';
 import Product from '../pages/Product';
 import Catalog from '../pages/Catalog';
 import RegisterProduct from '../pages/RegisterProduct';
-import Login from '../pages/Login';
 
 const Routes: React.FC = () => (
   <Switch>
@@ -13,7 +12,6 @@ const Routes: React.FC = () => (
     <Route path="/catalog/:page+" component={Catalog} />
     <Route path="/categories/:category+" component={Catalog} />
     <Route path="/register" component={RegisterProduct} />
-    <Route path="/login" component={Login} />
   </Switch>
 );
 
