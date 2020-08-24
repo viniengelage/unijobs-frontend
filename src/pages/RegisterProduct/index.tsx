0c0c37230b873027ef207134ea8e71b3187720e4
Thainá Weingartner <65509827+thainaweingartner@users.noreply.github.com>
2020-08-24 20:40:49 -0300

    layout

--- src/pages/RegisterProduct/index.tsx
+++ src/pages/RegisterProduct/index.tsx
@@ -1,15 +1,15 @@
 import React, {useCallback} from 'react';
 import {Form} from '@unform/web';
-import {IoMdCube,IoMdCode,IoMdPricetag, IoMdFolder,IoMdDocument, IoMdPeople, IoMdSchool, IoIosMail} from 'react-icons/io';
+//import {IoMdCube,IoMdCode,IoMdPricetag, IoMdFolder,IoMdDocument, IoMdPeople, IoMdSchool, IoIosMail} from 'react-icons/io';
 import * as Yup from 'yup';
 
 import api from '../../services/api'
 
-import {Container} from './styles';
+import {ContainerRoot, Title, Label, Input, Formbox, Button, Select, TextArea} from './styles';
 import Banner from '../../components/Banner';
-
-import Input from '../../components/Input';
-import Button from '../../components/Button';
+import Footer from '../../components/Footer';
+//import Input from '../../components/Input';
+//import Button from '../../components/Button';
 
 const RegisterProduct: React.FC = () => {
   const handleCreateProduct = useCallback(async(data)=>{
@@ -18,14 +18,12 @@ const RegisterProduct: React.FC = () => {
       description: data.description,
       price: data.price,
       type: data.type,
-      image: [data.image1, data.image2],
-      name: data.name,
-      course: data.course,
-      contact: data.contact
+      image: data.images,
     }
 
     api.post('/items', product).then(()=>(alert("Produto Adicionado!")))
   },[]);
+
   const handleSubmit = useCallback(async(data: object) =>{
     try{
       const schema = Yup.object().shape({
@@ -35,9 +33,6 @@ const RegisterProduct: React.FC = () => {
         image1: Yup.string().required(),
         image2: Yup.string().required(),
         type: Yup.string().required(),
-        name: Yup.string().required(),
-        course: Yup.string().required(),
-        contact: Yup.string().required(),
       })
 
       await schema.validate(data, {
@@ -48,27 +43,48 @@ const RegisterProduct: React.FC = () => {
     }catch(err){
       alert(err);
     }
+    
   },[handleCreateProduct]);
   return(
     <>
-      <Banner backIcon={false}/>
-      <Container>
+    <div style={{width:'100vw', height:'100vh'}}>
+      <Banner backIcon={true}/>
+      <ContainerRoot>
         <Form onSubmit={handleSubmit} >
-          <Input name="title" icon={IoMdCube} placeholder="Titulo do produto"/>
-          <Input name="description" icon={IoMdCode} placeholder="Descrição do produto"/>
-          <Input name="price" type="number" icon={IoMdPricetag} placeholder="Preço"/>
-          <Input name="image1" type="text" icon={IoMdFolder} placeholder="Link Imagem 1"/>
-          <Input name="image2" type="text" icon={IoMdFolder} placeholder="Link Imagem 2"/>
-          <Input name="type" icon={IoMdDocument} placeholder="Tipo"/>
-          <div style={{marginBottom: '10px', marginTop: '-2px'}}>
-            <span>Use apenas <strong>"produto"</strong> ou <strong>"servico"</strong> sem letras <strong>maisculas</strong> ou <strong>ç</strong>.</span>
-          </div>
-          <Input name="name" icon={IoMdPeople} placeholder="Nome do usuário"/>
-          <Input name="course" icon={IoMdSchool} placeholder="Curso"/>
-          <Input name="contact" icon={IoIosMail} placeholder="Contato"/>
-          <Button type="submit">Cadastrar Produto</Button>
+          <Formbox >
+            <Title>Adicionar Produto</Title>
+            
+            <Label>Titulo</Label>
+            <Input name="title"type='text'/>
+
+            <div className="infos" style={{display:'flex'}}>
+              <div>
+                <Label>Preço</Label>
+                <Input name="price" type="number" style={{width:'200px'}}/>
+              </div>
+              <div>
+                <Label>Categoria:</Label>
+                  <Select>
+                    <option value="0">Produto</option>
+                    <option value="1">Serviço</option>
+                  </Select>
+              </div>
+            </div> 
+            
+            <Label>Imagens</Label>
+            <Input name="images" type="file" multiple/>
+
+            <Label>Descrição</Label>
+            <TextArea name="description" />
+            
+            <Button type="submit">Salvar</Button>
+            <Button type="button" style={{background:'#F5F6F7',border:'1px solid #CCD0D5',color:'#767676'}}>Cancelar</Button>
+          </Formbox>
         </Form>
-      </Container>
+      </ContainerRoot>
+      <Footer />
+    </div>
+      
     </>
   );
 }
