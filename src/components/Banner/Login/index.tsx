a849b4f139a0f30d227f2a55ad8d7349ac4c2be5
Thainá Weingartner <65509827+thainaweingartner@users.noreply.github.com>
2020-09-03 17:55:50 -0300

    header para opção login e logout e ajuste de card categorias

---
+++ src/components/Banner/Login/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+import { FiChevronLeft } from 'react-icons/fi';
+
+import { Header } from './styles';
+import logo from '../../../assets/logo.svg';
+
+interface BannerProps {
+  backIcon: boolean;
+}
+
+const BannerLogin: React.FC<BannerProps> = ({ backIcon }) => {
+  const history = useHistory();
+
+  function renderBackIcon(value: boolean) {
+    if (value) {
+      return (
+        <button type="button" onClick={history.goBack}>
+          <FiChevronLeft size={35} />
+        </button>
+      );
+    }
+  }
+
+  return (
+    <Header>
+      {renderBackIcon(backIcon)}
+      <img src={logo} alt="Logo UniJobs" />
+    </Header>
+  );
+};
+
+export default BannerLogin;
