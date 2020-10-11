dc46d39681d6f947133040f6216e1e9c0c5b7b87
Thainá Weingartner <65509827+thainaweingartner@users.noreply.github.com>
2020-10-10 21:25:52 -0300

    lógica header

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 
 import { FiChevronLeft } from 'react-icons/fi';
 
 import { Header } from './styles';
 import logo from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
 
 interface BannerProps {
   backIcon: boolean;
@@ -12,6 +13,7 @@ interface BannerProps {
 
 const Banner: React.FC<BannerProps> = ({ backIcon }) => {
   const history = useHistory();
+  const { signOut } = useAuth();
 
   function renderBackIcon(value: boolean) {
     if (value) {
@@ -23,11 +25,30 @@ const Banner: React.FC<BannerProps> = ({ backIcon }) => {
     }
   }
 
+  const handleClick = () => {
+    signOut();
+  };
+
   return (
     <Header>
       {renderBackIcon(backIcon)}
-      <img src={logo} alt="Logo UniJobs" />
+      <Link to="/">
+        <img src={logo} alt="Logo UniJobs" />
+      </Link>
       {/* <input placeholder="Pesquise um item..." /> */}
+      {useAuth() ? (
+        <div>
+          <a href="/RegisterUser">Quero Vender</a>
+          <a href="/Login">Login</a>
+        </div>
+      ) : (
+        <div>
+          <a href="/RegisterUser">Quero Vender</a>
+          <Link to="/" onClick={handleClick}>
+            Sair
+          </Link>
+        </div>
+      )}
     </Header>
   );
 };
