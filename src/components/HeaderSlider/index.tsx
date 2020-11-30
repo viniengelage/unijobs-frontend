b376971d49875717108883127520c3af2e740202
Catherine Korres <catherine@catherinekorres.dev>
2020-11-30 20:10:04 -0300

    update HeaderSlider with new selected images

--- src/components/HeaderSlider/index.tsx
+++ src/components/HeaderSlider/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Flickity from 'react-flickity-component';
 import './styles.css';
 
-import imgOne from '../../assets/trends.svg';
+import imgOne from '../../assets/banner-1.png';
+import imgTwo from '../../assets/banner-2.png';
+import imgThree from '../../assets/banner-3.png';
+import imgFour from '../../assets/banner-4.png';
 
 const flickityOptions = {
   initialIndex: 0,
   prevNextButtons: true,
   pageDots: false,
+  wrapAround: true,
 };
 
 const Slider: React.FC = () => (
@@ -19,9 +23,10 @@ const Slider: React.FC = () => (
     reloadOnUpdate // default false
     static // default false
   >
-    <img src={imgOne} alt="Trends" />
-    <img src={imgOne} alt="Trends" />
-    <img src={imgOne} alt="Trends" />
+    <img src={imgOne} alt="Unijobs" />
+    <img src={imgTwo} alt="Unijobs" />
+    <img src={imgThree} alt="Unijobs" />
+    <img src={imgFour} alt="Unijobs" />
   </Flickity>
 );
 
